ec628aac2b6558cca7c02d371b3725ae83ee342a
ogontaro <ogontaro@gmail.com>
2016-08-06 16:42:53 +0900

    趣味モデルの簡単なテストの記述

--- spec/factories/habits.rb
+++ spec/factories/habits.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :habit do
-    motivation "MyText"
-    deadline "2016-07-15 21:51:48"
-    commit "MyText"
-    decla "MyString"
-    declaration "MyText"
+    title '毎日、走る'
+    motivation '痩せて、彼女をゲットする！'
+    deadline Date.parse('2016-12-24 00:00:00')
+    commit '体重を50kgにする'
+    declaration '痩せます！！！'
   end
 end
--- spec/models/habit_spec.rb
+++ spec/models/habit_spec.rb
@@ -1,10 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Habit, type: :model do
-  it '全パラメータがあれば有効な状態であること'
-  it 'タイトルが入力されていないときは無効な状態であること'
-  it 'モチベーションが入力されていないときは無効な状態であること'
-  it '締め切りが入力されていないときは無効な状態であること'
-  it '成果が入力されていないときは無効な状態であること'
+  it '全パラメータがあれば有効な状態であること' do
+    expect(FactoryGirl.build(:habit)).to be_valid
+  end
+  it 'タイトルが入力されていないときは無効な状態であること' do
+    habit = FactoryGirl.build(:habit, title: nil)
+    habit.valid?
+    expect(habit).to include('can\'t be blank')
+  end
+  it 'モチベーションが入力されていないときは無効な状態であること' do
+    habit = FactoryGirl.build(:habit, motivation: nil)
+    habit.valid?
+    expect(habit).to include('can\'t be blank')
+  end
+  it '締め切りが入力されていないときは無効な状態であること' do
+    habit = FactoryGirl.build(:habit, deadline: nil)
+    habit.valid?
+    expect(habit).to include('can\'t be blank')
+  end
+  it '成果が入力されていないときは無効な状態であること' do
+    habit = FactoryGirl.build(:habit, commit: nil)
+    habit.valid?
+    expect(habit).to include('can\'t be blank')
+  end
   it 'ユーザと紐付いていないときは無効な状態であること'
 end

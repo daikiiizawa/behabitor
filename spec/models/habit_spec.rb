b9532697807c7252db7d7f71c895326ab56dd0d9
ogontaro <ogontaro@gmail.com>
2016-08-06 16:47:27 +0900

    FactoryGirlをincludeし、FacotryGirlクラスを省力して記述できるように設定

--- spec/models/habit_spec.rb
+++ spec/models/habit_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Habit, type: :model do
   it '全パラメータがあれば有効な状態であること' do
-    expect(FactoryGirl.build(:habit)).to be_valid
+    expect(build(:habit)).to be_valid
   end
   it 'タイトルが入力されていないときは無効な状態であること' do
-    habit = FactoryGirl.build(:habit, title: nil)
+    habit = build(:habit, title: nil)
     habit.valid?
     expect(habit).to include('can\'t be blank')
   end
   it 'モチベーションが入力されていないときは無効な状態であること' do
-    habit = FactoryGirl.build(:habit, motivation: nil)
+    habit = build(:habit, motivation: nil)
     habit.valid?
     expect(habit).to include('can\'t be blank')
   end
   it '締め切りが入力されていないときは無効な状態であること' do
-    habit = FactoryGirl.build(:habit, deadline: nil)
+    habit = build(:habit, deadline: nil)
     habit.valid?
     expect(habit).to include('can\'t be blank')
   end
   it '成果が入力されていないときは無効な状態であること' do
-    habit = FactoryGirl.build(:habit, commit: nil)
+    habit = build(:habit, commit: nil)
     habit.valid?
     expect(habit).to include('can\'t be blank')
   end
